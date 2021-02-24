417a24588d51dffb0500477af7b96a604ce4f1a4
Mathieu Besson <mathieubesson01@orange.fr>
2021-02-24 18:01:07 +0100

    Edit Pin and create PinType to build forms

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,40 +3,83 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+
+    private EntityManagerInterface $manager; 
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
+
+    #[Route('/', name: 'app_home', methods:['GET'])]
     public function index(PinRepository $repository): Response
     {
-
-        $manager = $this->get('doctrine')->getManager();
-
-        // dd($manager);
-        $pin = new Pin();
-
-        $pin->setTitle('dsbdshbdsbhqbdsqd');
-        $pin->setDescription('dsbdshbdsbhqbdsqd');
-
-        $manager->persist($pin);
-        $manager->flush();
-
-            die;
-
-
         $pins = $repository->findBy([], ['updatedAt' => 'DESC']);
 
         return $this->render('pins/index.html.twig', compact('pins'));
     }
 
 
-    #[Route('/pins/{id<[0-9]+>}', name: 'app_pins_show')]
+    #[Route('/pins/{id<[0-9]+>}', name: 'app_pins_show', methods:['GET'])]
     public function show(Pin $pin): Response
     {
         return $this->render('pins/show.html.twig', compact('pin'));
+    }
+
+    #[Route('/pins/create', name: 'app_pins_create', methods:['GET', 'POST'])]
+    public function create(Request $request): Response
+    {
+
+        $pin = new Pin();
+        $form = $this->createForm(PinType::class ,$pin);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $this->manager->persist($pin);
+            $this->manager->flush();
+            return $this->redirectToRoute('app_pins_show', ['id' => $pin->getId()]);
+        }
+
+        return $this->render('pins/create.html.twig', 
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+
+
+    #[Route('/pins/{id<[0-9]+>}/edit', name: 'app_pins_edit', methods:['GET', 'PUT'])]
+    public function edit(Pin $pin, Request $request): Response
+    {
+        $form = $this->createForm(PinType::class, $pin, ['method' => 'PUT']);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $this->manager->flush();
+            return $this->redirectToRoute('app_pins_show', ['id' => $pin->getId()]);
+        }
+
+        return $this->render('pins/edit.html.twig', 
+            [
+                'pin' => $pin,
+                'form' => $form->createView()
+            ]
+        );
     }
 }
